5f4921827556e9bede80f96938097cfa24cbad8e
jmorat <jmorat@gmail.com>
2023-04-11 18:25:46 +0200

    first draft.

--- extension/color_trace.py
+++ extension/color_trace.py
@@ -6,12 +6,14 @@ import base64
 import inkex
 import io
 import PIL.Image
+import PIL.ImageDraw
+import PIL.ImagePath
 import numpy as np
 from inkex.transforms import Transform
 from inkex.elements import ShapeElement, Image, Group
 from inkex.paths import Path, CubicSuperPath
 from inkex.localization import inkex_gettext as _
-
+import math
 
 class Trace(inkex.EffectExtension):
     """Randomize the colours of all objects"""
@@ -71,44 +73,45 @@ class Trace(inkex.EffectExtension):
         objs = self._get_objects()
         img_node, image = self._get_image()
         image_bb = img_node.bounding_box()
+        image_black = PIL.Image.new(mode="RGB", size=image.size, color=(0, 0, 0))
 
-        # img_transform = Transform(scale=(1/self.svg.viewport_width, 1/self.svg.viewport_height))
-            #translate=(-image_bb.left, -image_bb.top),
+        mask = PIL.Image.new("L", image.size, 0)
+        mask_canvas = PIL.ImageDraw.Draw(mask)
+        scale = np.array([
+            image.width / image_bb.width,
+            image.height / image_bb.height
+        ])
+        translate = np.array([
+            -image_bb.top,
+            -image_bb.left
+        ])
+
         # TODO: RASTER Path to image mask
         for shape in objs:
+            shape.apply_transform()
             shape_bb = shape.bounding_box()
             path = shape.path.to_superpath()
-            print(f'SHAPE::{shape.eid:10} :: {shape.TAG:10} // pos=[{shape_bb.center}] @ {shape_bb.size}')
+            # print(f'SHAPE::{shape.eid:10} :: {shape.TAG:10} // pos=[{shape_bb.center}] @ {shape_bb.size}')
+            mask_canvas.rectangle([(0, 0), mask.size], fill='black')
             for i, subpath in enumerate(path):
-                ww = np.array(subpath)
-                ww[:, 0, :] = ww[:, 1, :]
-                ww[:, 2, :] = ww[:, 1, :]
-                print(ww[:, 1, :])
-                path[i] = ww.tolist()
-            shape.style['fill'] = 'red'
+                xys = np.array(subpath)[:, 1, :]
+                xys = (xys +translate) * scale
+                polygon = [tuple(p) for p in xys]
+                mask_canvas.polygon(polygon, fill="#ffffff", outline="black")
+                image_composed = PIL.Image.composite(image, image_black, mask)
+                # image_composed.show()
+            color_average = np.sum(np.asarray(image_composed), axis=(0, 1)) / np.count_nonzero(np.asarray(mask))
+            color_average = color_average.astype(int)
+            color_print = f'#{color_average[0]:02x}{color_average[1]:02x}{color_average[2]:02x}'
+            # print(color_print)
+            shape.style['fill'] = color_print
             shape.path = path
-            # print(f'SHAPE::{shape.eid:10} :: {shape.TAG:10} // pos=[{shape.x}]')
-            # for p in e.path:
-            #     print(p)
-            # point = shape_bb.center
-            # point2 = img_transform.apply_to_point(point)
-            # print(f'{point} ==> {point2}')
-            # trans = e.composed_transform()
-            # point = trans.apply_to_point(point)
-            # point = tuple(int(p) for p in point)
-            # print(point)
-            # color = image.getpixel(point)
-            # color = f'#{color[0]:02x}{color[1]:02x}{color[1]:02x}'
-            # print(color)
-            # e.style['fill'] = color
-
-        # image.show()
 
 
 if __name__ == "__main__":
     if True:
-        input_file = r'D:\devel\inkex_color_trace\sample\drawing.svg'
-        output_file = r'D:\devel\inkex_color_trace\sample\result.svg'
+        input_file = r'/local/devel/inkex_color_trace/sample/drawing.svg'
+        output_file = r'/local/devel/inkex_color_trace/sample/result.svg'
         Trace().run([input_file, '--output=' + output_file])
     else:
         Trace().run()
